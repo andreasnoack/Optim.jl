b5b5169878ef34f83c72298cdec35fab115b6e54
Andreas Noack <andreasnoackjensen@gmail.com>
2017-04-04 12:54:40 -0400

    Avoid temporaries in update_state! by carrying buffer in state struct

--- src/newton_trust_region.jl
+++ src/newton_trust_region.jl
@@ -1,9 +1,151 @@
+using Base.LinAlg: BlasInt, BlasReal, BlasFloat, checksquare, chkstride1
+using Base.LinAlg.BLAS: @blasfunc
+using Base.LinAlg.LAPACK: chklapackerror
+
+# for (geev, elty) in
+#     ((:dgeev_,:Float64),
+#      (:sgeev_,:Float32))
+#     @eval begin
+#         #      SUBROUTINE DGEEV( JOBVL, JOBVR, N, A, LDA, WR, WI, VL, LDVL, VR,
+#         #      $                  LDVR, WORK, LWORK, INFO )
+#         # *     .. Scalar Arguments ..
+#         #       CHARACTER          JOBVL, JOBVR
+#         #       INTEGER            INFO, LDA, LDVL, LDVR, LWORK, N
+#         # *     .. Array Arguments ..
+#         #       DOUBLE PRECISION   A( LDA, * ), VL( LDVL, * ), VR( LDVR, * ),
+#         #      $                   WI( * ), WORK( * ), WR( * )
+#         function geev!(jobvl::Char, jobvr::Char, A::StridedMatrix{$elty}, VL::StridedMatrix{$elty}, VR::StridedMatrix{$elty}, WR::StridedVector{$elty}, WR::StridedVector{$elty}, work::Vector{$elty}, query::Bool = false)
+
+#             chkstride1(A)
+#             n = checksquare(A)
+#             chkfinite(A) # balancing routines don't support NaNs and Infs
+#             lwork = query ? -1 : length(work) # is it a query for work space?
+
+#             @assert length(WR) == length(WI) == n
+#             @assert jobvl == 'N' || (jobvl == 'V' && size(VL, 1) == n && size(VL, 2) == n)
+#             @assert jobvr == 'N' || (jobvr == 'V' && size(VR, 1) == n && size(VR, 2) == n)
+#             @assert lwork >= query ? -1 : max(1, 3*n)
+
+#             info  = Ref{BlasInt}()
+
+#             ccall((@blasfunc($geev), liblapack), Void,
+#                 (Ptr{UInt8}, Ptr{UInt8}, Ptr{BlasInt}, Ptr{$elty},
+#                  Ptr{BlasInt}, Ptr{$elty}, Ptr{$elty}, Ptr{$elty},
+#                  Ptr{BlasInt}, Ptr{$elty}, Ptr{BlasInt}, Ptr{$elty},
+#                  Ptr{BlasInt}, Ptr{BlasInt}),
+#                  &jobvl, &jobvr, &n, A,
+#                  &max(1,stride(A,2)), WR, WI, VL,
+#                  &n, VR, &n, work,
+#                  &lwork, info)
+#             end
+#             chklapackerror(info[])
+#             return WR, WI, VL, VR
+#         end
+
+#         function geev!(jobvl::Char, jobvr::Char, A::StridedMatrix{$elty})
+
+#             n = checksquare(A)
+
+#             @assert jobvl == 'N' || jobvl == 'V'
+#             @assert jobvr == 'N' || jobvr == 'V'
+
+#             WR = Vector{$elty}(n)
+#             WI = Vector{$elty}(n)
+#             if jobvl == 'V'
+#                 VL = Matrix{$elty}(n,n)
+#             else
+#                 VL = Matrix{$elty}(0,0)
+#             end
+#             if jobvr == 'V'
+#                 VR = Matrix{$elty}(n,n)
+#             else
+#                 VR = Matrix{$elty}(0,0)
+#             end
+#             work = Vector{BlasInt}(1)
+
+#             # Query optimal size
+#             geev!(jobvl, jobvr, A, VL, VR, WR, WR, work, true)
+#             resize!(work, Int(work[1]))
+
+#             return geev!(jobvl, jobvr, A, VL, VR, WR, WR, work)
+#         end
+#     end
+# end
+
+syev_worksize(::Type{T}, n::Integer) where {T<:BlasFloat} = ccall((@blasfunc(ilaenv_), Base.liblapack_name), Int,
+    (Ptr{Int}, Ptr{UInt8}, Ptr{UInt8}, Ptr{Int}, Ptr{Int}, Ptr{Int}, Ptr{Int}),
+        &1, (T === Float32 ? "s" : (T === Float64 ? "d" : (T === Complex{Float64} ? "c" : "z")))*"sytrd",
+            "V", &n, &(-1), &(-1), &(-1))
+
+for (syev, elty) in
+    ((:dsyev_,:Float64),
+     (:ssyev_,:Float32))
+    @eval begin
+        #       SUBROUTINE DSYEV( JOBZ, UPLO, N, A, LDA, W, WORK, LWORK, INFO )
+        # *     .. Scalar Arguments ..
+        #       CHARACTER          JOBZ, UPLO
+        #       INTEGER            INFO, LDA, LWORK, N
+        # *     .. Array Arguments ..
+        #       DOUBLE PRECISION   A( LDA, * ), W( * ), WORK( * )
+        function syev!(jobz::Char, uplo::Char, A::StridedMatrix{$elty}, W::Vector{$elty}, work::Vector{$elty}, query::Bool = false)
+            chkstride1(A)
+            n = checksquare(A)
+            lwork = query ? -1 : length(work)
+
+            @assert jobz == 'N' || jobz == 'V'
+            @assert uplo == 'U' || uplo == 'L'
+            @assert length(W) == n
+            @assert query || lwork >= max(1, 3*n - 1)
+
+            info  = Ref{BlasInt}()
+            ccall((@blasfunc($syev), Base.liblapack_name), Void,
+                (Ptr{UInt8}, Ptr{UInt8}, Ptr{BlasInt}, Ptr{$elty}, Ptr{BlasInt},
+                 Ptr{$elty}, Ptr{$elty}, Ptr{BlasInt}, Ptr{BlasInt}),
+                 &jobz, &uplo, &n, A, &max(1,stride(A,2)),
+                 W, work, &lwork, info)
+            chklapackerror(info[])
+            return W, A, work
+        end
+
+        function syev!(jobz::Char, uplo::Char, A::StridedMatrix{$elty})
+            chkstride1(A)
+            n = checksquare(A)
+
+            W    = Vector{$elty}(n)
+            work = Vector{$elty}(3*n - 1)
+
+            syev!(jobz, uplo, A, W, work, true)
+            resize!(work, Int(work[1]))
+            return syev!(jobz, uplo, A, W, work)
+        end
+    end
+end
+
+immutable SymmetricEigen{T<:Real}
+    values::Vector{T}
+    vectors::Matrix{T}
+    workbuffer::Vector{T}
+end
+
+eigfact2!(A::Symmetric{<:BlasReal,<:StridedMatrix}) = SymmetricEigen(syev!('V', A.uplo, A.data)...)
+function eigfact2!(F::SymmetricEigen{T}, A::Symmetric{T}) where T<:BlasReal
+    copy!(F.vectors, A.data)
+    syev!('V', A.uplo, F.vectors, F.values, F.workbuffer)
+    return F
+end
+eigfact2(A::Symmetric{<:Real,<:StridedMatrix}) = eigfact2!(copy(A))
+
 type NewtonTrustRegionState{T,N,G}
     @add_generic_fields()
     x_previous::Array{T,N}
     g_previous::G
     f_x_previous::T
     s::Array{T,N}
+    q_l::Array{T,N}
+    H_ridged::Matrix{T}
+    qg::Vector{T}
+    H_eig::SymmetricEigen{T}
+    F::LinAlg.Cholesky{T,Matrix{T}}
     hard_case::Bool
     reached_subproblem_solution::Bool
     interior::Bool
@@ -55,13 +197,25 @@ end
 function p_sq_norm{T}(λ::T, min_i, n, qg, H_eig)
     p_sum = zero(T)
     for i = min_i:n
-        p_sum += qg[i]^2 / (λ + H_eig[:values][i])^2
+        p_sum += qg[i]^2 / (λ + H_eig.values[i])^2
     end
     p_sum
 end
 
-eigfact2(A::Symmetric{T} where T <: Real) =
-    LinAlg.Eigen{eltype(A),real(eltype(A)),Matrix{eltype(A)},Vector{real(eltype(A))}}(LAPACK.syev!('V', 'L', copy(A.data))...)
+function Base.LinAlg.cholfact!(C::LinAlg.Cholesky{T,S}, A::Hermitian{T,S}) where {T,S}
+    if C.uplo != A.uplo
+        throw(ArgumentError("uplo fields are not the same"))
+    end
+    copy!(C.factors, A.data)
+    cholfact!(Hermitian(C.factors, Symbol(A.uplo)))
+    return C
+end
+function getR(C::LinAlg.Cholesky)
+    if C.uplo == :L
+        throw(ArgumentError("only possible to extract R factor when factorization is stored in upper triangle"))
+    end
+    return UpperTriangular(C.factors)
+end
 
 # Choose a point in the trust region for the next step using
 # the interative (nearly exact) method of section 4.3 of Nocedal and Wright.
@@ -101,29 +255,26 @@ function solve_tr_subproblem!{T}(gr::Vector{T},
     # Note that currently the eigenvalues are only sorted if H is perfectly
     # symmetric.  (Julia issue #17093)
     # H_eig = eigfact(Symmetric(H))
-    H_eig = eigfact2(Symmetric(H))
-    min_H_ev, max_H_ev = H_eig[:values][1], H_eig[:values][n]
-    H_ridged = copy(Symmetric(H))
+    H_eig = state.H_eig
+    eigfact2!(H_eig, Symmetric(H))
+    min_H_ev, max_H_ev = H_eig.values[1], H_eig.values[n]
+    H_ridged           = state.H_ridged
+    copy!(H_ridged, H)
 
     # Cache the inner products between the eigenvectors and the gradient.
-    # qg = similar(gr)
-    # for i=1:n
-        # qg[i] = vecdot(H_eig[:vectors][:, i], gr)
-    # end
-    qg = H_eig[:vectors]'*gr
+    qg = Ac_mul_B!(state.qg, H_eig.vectors, gr)
 
     # These values describe the outcome of the subproblem.  They will be
     # set below and returned at the end.
-    interior = true
-    hard_case = false
+    interior         = true
+    hard_case        = false
     reached_solution = true
 
     if min_H_ev >= 1e-8 && p_sq_norm(zero(T), 1, n, qg, H_eig) <= δ_sq
         # No shrinkage is necessary: -(H \ gr) is the minimizer
         interior = true
         reached_solution = true
-        # s[:] = -(H_eig[:vectors] ./ H_eig[:values]') * H_eig[:vectors]' * gr
-        BLAS.gemv!('N', -one(T), H_eig[:vectors], qg ./ H_eig[:values], zero(T), s)
+        BLAS.gemv!('N', -one(T), H_eig.vectors, qg ./ H_eig.values, zero(T), s)
         λ = zero(T)
     else
         interior = false
@@ -131,7 +282,7 @@ function solve_tr_subproblem!{T}(gr::Vector{T},
         # The hard case is when the gradient is orthogonal to all
         # eigenvectors associated with the lowest eigenvalue.
         hard_case_candidate, min_H_ev_multiplicity =
-            check_hard_case_candidate(H_eig[:values], qg)
+            check_hard_case_candidate(H_eig.values, qg)
 
         # Solutions smaller than this lower bound on λ are not allowed:
         # they don't ridge H enough to make H_ridge PSD.
@@ -160,10 +311,10 @@ function solve_tr_subproblem!{T}(gr::Vector{T},
                 # I don't think it matters which eigenvector we pick so take
                 # the first.
                 for i=1:n
-                    s[i] = tau * H_eig[:vectors][i, 1]
+                    s[i] = tau * H_eig.vectors[i, 1]
                     for k = (min_H_ev_multiplicity + 1):n
                         s[i] = s[i] +
-                               qg[k] * H_eig[:vectors][i, k] / (H_eig[:values][k] + λ)
+                               qg[k] * H_eig.vectors[i, k] / (H_eig.values[k] + λ)
                     end
                 end
             end
@@ -178,17 +329,18 @@ function solve_tr_subproblem!{T}(gr::Vector{T},
             end
 
             reached_solution = false
-            q_l = similar(s)
+            q_l              = state.q_l
+            H_ridgedHerm     = Hermitian(H_ridged, :U)
+            F                = state.F
             for iter in 1:max_iters
                 λ_previous = λ
 
-                R = chol(Hermitian(H_ridged))
+                cholfact!(F, H_ridgedHerm)
+                R = getR(F)
                 @inbounds for i in eachindex(gr)
                     s[i] = -gr[i]
                 end
                 A_ldiv_B!(R, Ac_ldiv_B!(R, s))
-                # s[:] = -R \ (R' \ gr)
-                # q_l = R' \ s
                 copy!(q_l, s)
                 Ac_ldiv_B!(R, q_l)
                 norm2_s = dot(s, s)
@@ -259,6 +411,11 @@ function initial_state{T}(method::NewtonTrustRegion, options, d, initial_x::Arra
                          similar(gradient(d)), # Store previous gradient in state.g_previous
                          T(NaN), # Store previous f in state.f_x_previous
                          similar(initial_x), # Maintain current search direction in state.s
+                         similar(initial_x), # buffer of same type and size as stats.s
+                         Matrix{T}(n,n),     # buffer of H_ridged
+                         similar(initial_x), # buffer qg
+                         SymmetricEigen{T}(Vector{T}(n), Matrix{T}(n,n), Vector{BlasInt}((syev_worksize(T, n) + 2)*n)), # worksize only correct for syev solver
+                         LinAlg.Cholesky{T,Matrix{T}}(Matrix{T}(n,n), 'U'), # buffer for Cholesky
                          hard_case,
                          reached_subproblem_solution,
                          interior,
